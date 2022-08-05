17e4018220c0adfe58480b4abebf323bee2de62b
Nadi Duno <nadiduno@gmail.com>
2022-08-04 23:16:00 -0300

    fourth commit

--- codigobase.py
+++ codigobase.py
@@ -6,8 +6,8 @@ df = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases
 
 df = df.rename(columns={'newDeaths': 'Novos óbitos','newCases': 'Novos casos','deaths_per_100k_inhabitants': 'Óbitos por 100 mil habitantes','totalCases_per_100k_inhabitants':'Casos por 100 mil habitantes'})
 
-state = st.selectbox('Qual estado?', estados)
 estados = list(df['state'].unique())
+state = st.selectbox('Qual estado?', estados)
 
 
 colunas = ['Novos óbitos','Novos casos','Óbitos por 100 mil habitantes','Casos por 100 mil habitantes']
